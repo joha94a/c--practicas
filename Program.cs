18ef1b975721fb5f8e4731a526f90e59c8fd4f2d
Johana <74420328+joha94a@users.noreply.github.com>
2022-04-21 18:11:03 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ejercicios_forwhile_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Hacer un programa que solicite el ingreso de 10 números y que muestre el mayor de ellos por pantalla. Solo se debe emitir UN valor por pantalla.
+
+            int numero, max=0;
+            
+            for (int x = 0; x < 10; x++)
+            {
+                Console.WriteLine("Ingresa un número:");
+                numero = int.Parse(Console.ReadLine());
+                
+                if(x == 0) 
+                max = numero;
+
+                else if(numero > max)
+                max = numero; 
+            }
+
+        
+            Console.WriteLine("El numero mayor es:" + max);
+
+        }
+    }
+}
